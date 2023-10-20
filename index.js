5502210ab1d729df692ab1cbbcf9c947a7ec4be4
Alois Mbutura <aloismbutura@gmail.com>
2023-10-20 23:50:59 +0000

    revamp code to make more modern as promises are supported

--- index.js
+++ index.js
@@ -1,56 +1,46 @@
-// const { reporters } = require('mocha');
-const { async } = require('q');
-var Q = require('q');
-
-
 exports.processData = async function (buf) {
-    var deferred = Q.defer()
+    let PROTOCOL = require('./lib/protocol.js')
+    let driver = new PROTOCOL()
 
+    let data = buf
 
-    try {
-        let PROTOCOL = require('./lib/protocol.js')
-        let driver = new PROTOCOL()
-
-        let data = buf
-
-
-        args = {
-            "rtuId": -1, "data": data, "originalData": data, "values": {
-                time: 0,
-                TxFlag: 0,
-                digitalsIn: 0,
-                digitalsOut: 0,
-                AI1: 0,
-                AI2: 0,
-                AI3: 0,
-                AI4: 0,
-                AIExt1: 0,
-                AIExt2: 0,
-                AIExt3: 0,
-                AIExt4: 0,
-                AIExt5: 0,
-                AIExt6: 0,
-                AIExt7: 0,
-                AIExt8: 0,
-                CI1: 0,
-                CI2: 0,
-                CI3: 0,
-                CI4: 0,
-                CI5: 0,
-                CI6: 0,
-                CI7: 0,
-                CI8: 0,
-                BATT: 0,
-                SIG: 0,
-                ExternalVoltage: 0,
-                InternalTemperature: 0
-            }
+    args = {
+        "rtuId": -1, "data": data, "originalData": data, "values": {
+            time: 0,
+            TxFlag: 0,
+            digitalsIn: 0,
+            digitalsOut: 0,
+            AI1: 0,
+            AI2: 0,
+            AI3: 0,
+            AI4: 0,
+            AIExt1: 0,
+            AIExt2: 0,
+            AIExt3: 0,
+            AIExt4: 0,
+            AIExt5: 0,
+            AIExt6: 0,
+            AIExt7: 0,
+            AIExt8: 0,
+            CI1: 0,
+            CI2: 0,
+            CI3: 0,
+            CI4: 0,
+            CI5: 0,
+            CI6: 0,
+            CI7: 0,
+            CI8: 0,
+            BATT: 0,
+            SIG: 0,
+            ExternalVoltage: 0,
+            InternalTemperature: 0
         }
+    }
 
-        let response = args
-        response.data = data
-        response.arrBufAllData = []
-
+    let response = args
+    response.data = data
+    response.arrBufAllData = []
+    try {
         response = await driver.splitRawData(response)
         for (let i = 0; i < response.arrBufRawData.length; i++) {
             response.message = response.arrBufRawData[i]
@@ -134,32 +124,25 @@ exports.processData = async function (buf) {
                 arrShapedData.push(shapedData)
             }))
             response.arrShapedData = arrShapedData
-            deferred.resolve(response)
-        }else{
-            deferred.resolve(response)
         }
     } catch (e) {
-        console.error('digitalMattersFalcon2GDriver processData Error', e)
-        deferred.reject(e)
+        console.error('digitalMattersFalcon2GDriver processData Error', e);
+        throw new Error(e);
     }
 
-    return deferred.promise
+    return response;
 }
 
 exports.processTime = async function (digitalMattersTS) {
-    var deferred = Q.defer()
-
     let PROTOCOL = require('./lib/protocol.js')
     let driver = new PROTOCOL()
 
     try {
         let dmDate = await driver.processTime(digitalMattersTS)
-        deferred.resolve(dmDate)
     } catch (e) {
         console.error('digitalMattersFalcon2GDriver processTime Error', e)
-        deferred.reject(e)
+        throw new Error(e);
     }
 
-    return deferred.promise
-
+    return dmDate
 }
